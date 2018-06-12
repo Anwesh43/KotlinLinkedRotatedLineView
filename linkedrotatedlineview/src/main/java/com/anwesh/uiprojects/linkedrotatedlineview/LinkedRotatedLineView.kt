6f0aa1f3eaf7352465b83f9350182c4f753d88a3
Anwesh43 <anweshthecool0@gmail.com>
2018-06-12 09:58:54 +0530

    adding LinkedRotatedLine class

--- linkedrotatedlineview/src/main/java/com/anwesh/uiprojects/linkedrotatedlineview/LinkedRotatedLineView.kt
+++ linkedrotatedlineview/src/main/java/com/anwesh/uiprojects/linkedrotatedlineview/LinkedRotatedLineView.kt
@@ -135,4 +135,28 @@ class LinkedRotatedLineView (ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedRotatedLine(var i : Int) {
+
+        private var curr : RotatedLineNode = RotatedLineNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
